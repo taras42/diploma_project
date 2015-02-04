8aa213642e6cc5217fd791769abd01e1187c00db
taras42 <taras.my.mail@gmail.com>
2015-02-04 22:11:48 +0200

    Basic Grid Implementation.

---
+++ web/src/components/common/GridOverlay/GridOverlay.js
@@ -0,0 +1,58 @@
+define(function(require){
+
+    var Backbone = require('backbone'),
+        _ = require('underscore'),
+        $ = require('jquery'),
+        gridTemplate = require("text!./template/gridTemplate.htm"),
+        ItemCollection = require("components/common/itemCollection/ItemCollection");
+
+
+    var GridOverlay = Backbone.View.extend({
+        el: gridTemplate,
+
+        constructor: function(options){
+
+            this.parentElement = options.parentElement ?  $(options.parentElement) : $('body');
+
+            this.cellsCollection = new ItemCollection({
+                additionalCssClass: options.gridAdditionCssClass || "",
+                itemAdditionalCssClass: options.cellAdditionCssClass || "",
+                itemTemplate: options.cellTemplate,
+                model: options.model,
+                items: options.data || []
+            });
+
+            Backbone.View.apply(this, arguments);
+        },
+
+        initialize: function() {
+            this.initEvents();
+        },
+
+        initEvents: function() {
+            this.listenTo(this.cellsCollection, "item:selected", this.onCellClick);
+        },
+
+        onCellClick:function(cellView, model) {
+            this.trigger("cell:selected", cellView, model);
+        },
+
+        renderData: function(data) {
+            var self = this;
+            _.each(this.cellCollection.models, function(model){
+                var item = self.cellsCollection.addItem(model);
+                self.cellsCollection.renderItem(item);
+            });
+        },
+
+        render: function(){
+            this.$el.append(this.cellsCollection.render().$el);
+            return this;
+        }
+
+    });
+
+
+    return GridOverlay;
+
+});
